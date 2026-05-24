af0216730eaaf1f8aa8b6bfde62b86a6899c2e51
atrminhocao-star <atrminhocao@gmail.com>
2026-05-24 14:30:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,13 @@
 
+{
+  "usuarios": [],
+  "clientes": [],
+  "materiais": [],
+  "caminhoes": [],
+  "viagens": [],
+  "despesas": [],
+  "entradasCaixa": [],
+  "saidasManuais": [],
+  "contasReceberFixas": [],
+  "exportadoEm": "2026-05-20T21:17:05.933Z"
+}
